b1728a3a0a34e461757333051e0962640369ee79
Lev Maximov <lev.maximov@gmail.com>
2010-12-16 22:37:32 +0000

    install.py for git-ff

--- install.py
+++ install.py
@@ -2,17 +2,30 @@
 
 import os, sys, shutil
 
-install_dir = '%s%s/after/ftplugin' % (
-    os.environ['HOME'],
-    'vimfiles' if sys.platform == 'win32' else '/.vim'
-)
+def install(filename, install_dir):
+    try:
+        os.makedirs(install_dir)
+    except OSError:
+        pass
 
-try:
-    os.makedirs(install_dir)
-except OSError:
-    pass
+    try:
+        shutil.copy(filename, install_dir)
+    except shutil.Error:
+        pass
 
-try:
-    shutil.copy('python.vim', install_dir)
-except shutil.Error:
-    pass
+def python_vim():
+    install('python.vim',
+        '%s%s/after/ftplugin' % (
+            os.environ['HOME'],
+            'vimfiles' if sys.platform == 'win32' else '/.vim'
+    ))
+
+
+def git_ff():
+    if sys.platform == 'win32':
+        return
+    install('git-ff', os.environ['HOME'] + '/bin/')
+
+if __name__ == '__main__':
+    python_vim()
+    git_ff()
