2aa1b0032c8170ad606ec726e2c349d1b7812548
Lev Maximov <lev.maximov@gmail.com>
2010-12-17 12:36:36 +0600

    install.py fix

--- install.py
+++ install.py
@@ -3,20 +3,27 @@
 import os, sys, shutil
 
 def install(filename, install_dir):
+    print 'installing %s into %s' % (filename, install_dir)
     try:
         os.makedirs(install_dir)
+        print 'dir created'
     except OSError:
-        pass
+        print 'dir not created'
 
     try:
         shutil.copy(filename, install_dir)
+        print 'file copied'
     except shutil.Error:
-        pass
+        print 'file not copied'
 
 def python_vim():
+    home = os.environ['HOME']
+    if sys.platform == 'win32':
+        if home[-1] not in ['/', '\\']:
+            home += '/'
     install('python.vim',
         '%s%s/after/ftplugin' % (
-            os.environ['HOME'],
+            home,
             'vimfiles' if sys.platform == 'win32' else '/.vim'
     ))
 
