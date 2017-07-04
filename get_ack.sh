222ce15a09afe6dc361ce560ed5f084d860020b6
Lev Maximov <lev.maximov@gmail.com>
2017-07-04 19:38:37 +0100

    updated ack

--- get_ack.sh
+++ get_ack.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-curl https://beyondgrep.com/ack-2.16-single-file > ~/bin/ack && chmod 0755 ~/bin/ack
+curl https://beyondgrep.com/ack-2.18-single-file > ~/bin/ack && chmod 0755 ~/bin/ack
