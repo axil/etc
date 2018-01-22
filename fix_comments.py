4aa8ce5f7bba08ba9c2e05ec2e0355ad4eb85fa7
Lev Maximov <lev.maximov@gmail.com>
2018-01-22 18:38:44 +0700

    fix

--- fix_comments.py
+++ fix_comments.py
@@ -1,8 +1,9 @@
+import os
 filename = r'D:\Utilities\Development\Python\Lib\site-packages\notebook\static\notebook\js\main.min.js'
 os.rename(filename, filename+'~')
-fin, fout = open(filename+'~'), open(filename, 'w')
+fin, fout = open(filename+'~', encoding='utf8', newline=''), open(filename, 'w', encoding='utf8')
 for line in fin:
     fout.write(line.replace(
         'padding == null ? " "',
-        'padding == null ? ""')
+        'padding == null ? ""'))
 
