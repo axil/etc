d4745d8048e7447f10d4c31d3246d8f60f0b0b23
Lev Maximov <lev.maximov@gmail.com>
2019-07-29 17:14:34 +0000

    curl->wget

--- get_ack.sh
+++ get_ack.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-curl https://beyondgrep.com/ack-2.18-single-file > ~/bin/ack && chmod 0755 ~/bin/ack
+wget -o ~/bin/ack https://beyondgrep.com/ack-2.18-single-file && chmod 0755 ~/bin/ack
