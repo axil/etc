013d42039e8d15a0cd8739721bbcf2031beeb72b
Lev Maximov <lev.maximov@gmail.com>
2019-07-30 12:50:37 +0300

    bugfix

--- get_ack.sh
+++ get_ack.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-wget -o ~/bin/ack https://beyondgrep.com/ack-2.18-single-file && chmod 0755 ~/bin/ack
+wget -O ~/bin/ack https://beyondgrep.com/ack-2.18-single-file && chmod 0755 ~/bin/ack
